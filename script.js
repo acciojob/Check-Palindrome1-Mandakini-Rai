28bd750c380aeb30e4ec405103eb67cb38de0acb
Mandakini-Rai <115775774+Mandakini-Rai@users.noreply.github.com>
2023-05-12 17:36:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
 
 function palindrome(str){
-
+	 str = str.toLowerCase();
+      let i =0;
+	let j = str.length();
+	while(i<=j){
+		while(str[i]==" "){
+			i++;
+		}
+		while( str[j]==" ")j--;
+		if(str[i] != str[j]) return false;
+		i++;
+		j--;
+	}
+	return true
 }
 module.exports = palindrome
